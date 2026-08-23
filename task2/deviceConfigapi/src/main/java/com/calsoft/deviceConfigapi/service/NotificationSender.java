b0114c4892079b7f891bcc62a87708d82760cac1
Chaitanya Luniya <chaitanya.luniya@calsoftinc.com>
2026-08-24 00:37:29 +0530

    new repo for task2

--- task2/deviceConfigapi/src/main/java/com/calsoft/deviceConfigapi/service/NotificationSender.java
+++
@@ -1,8 +0,0 @@
-package com.calsoft.deviceConfigapi.service;
-
-import com.calsoft.deviceConfigapi.dto.DeviceConfigNotificationDTO;
-
-public interface NotificationSender {
-
-    void send(DeviceConfigNotificationDTO notification);
-}
